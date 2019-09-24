85f1b6e22af73e647c6b3ee4e8a0c65ba73e2a21
Tomasz Wyrwa <wyrwa.t@gmail.com>
2019-09-24 11:42:50 +0200

    complete

--- js/script.js
+++ js/script.js
@@ -1,21 +1,41 @@
 
-// 1 
+//1 
+console.log('1 exercise:');
 let hello = 'hello';
 let world = 'world';
 
 function sayHello() {
     console.log(`${hello} ${world}`)
 }
-
 sayHello();
 
 //2
-let a = 3;
-let b = 8;
+console.log('2 exercise:');
+const multiply = (a = 7, b = 5) => console.log(a * b);
+multiply(5,8);
+multiply(7);
+multiply();
 
-let multiplay = (a = 7, b = 5) => console.log(a * b);
+//3
+console.log('3 exercise:');
+let avarege = (...args) => {
+    let sum = 0;
+    for (let arg of args) {
+        sum += arg;
+    }
+    return sum / args.length;
+}
+console.log(avarege(1));
+console.log(avarege(1,3));
+console.log(avarege(1,3,6,6));
 
-multiplay(a);
-multiplay(b);
-multiplay(a, b);
+//4
+console.log('4 exercise:');
+const grades = [1, 5, 5, 5, 4, 3, 3, 2, 1];
+console.log(avarege(...grades));
 
+//5 
+console.log('5 exercise:');
+const data = [1, 4, 'Iwona', false, 'Nowak'];
+const [ , ,firstname, ,secondname] = data;
+console.log(firstname,secondname);
--- script-compiled.js
+++ script-compiled.js
@@ -1,25 +1,74 @@
 'use strict';
 
-// 1 
+//1 
+console.log('1 exercise:');
 var hello = 'hello';
 var world = 'world';
 
 function sayHello() {
     console.log(hello + ' ' + world);
 }
-
 sayHello();
 
 //2
-var a = 3;
-var b = 8;
-
-var multiplay = function multiplay() {
+console.log('2 exercise:');
+var multiply = function multiply() {
     var a = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 7;
     var b = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 5;
     return console.log(a * b);
 };
+multiply(5, 8);
+multiply(7);
+multiply();
 
-multiplay(a);
-multiplay(b);
-multiplay(a, b);
+//3
+console.log('3 exercise:');
+var avarege = function avarege() {
+    for (var _len = arguments.length, args = Array(_len), _key = 0; _key < _len; _key++) {
+        args[_key] = arguments[_key];
+    }
+
+    var sum = 0;
+    var _iteratorNormalCompletion = true;
+    var _didIteratorError = false;
+    var _iteratorError = undefined;
+
+    try {
+        for (var _iterator = args[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+            var arg = _step.value;
+
+            sum += arg;
+        }
+    } catch (err) {
+        _didIteratorError = true;
+        _iteratorError = err;
+    } finally {
+        try {
+            if (!_iteratorNormalCompletion && _iterator.return) {
+                _iterator.return();
+            }
+        } finally {
+            if (_didIteratorError) {
+                throw _iteratorError;
+            }
+        }
+    }
+
+    return sum / args.length;
+};
+console.log(avarege(1));
+console.log(avarege(1, 3));
+console.log(avarege(1, 3, 6, 6));
+
+//4
+console.log('4 exercise:');
+var grades = [1, 5, 5, 5, 4, 3, 3, 2, 1];
+console.log(avarege.apply(undefined, grades));
+
+//5 
+console.log('5 exercise:');
+var data = [1, 4, 'Iwona', false, 'Nowak'];
+var firstname = data[2],
+    secondname = data[4];
+
+console.log(firstname, secondname);
